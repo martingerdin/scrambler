b92b66fb6a4a8410090caa9f042d5e6742f02e6e
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-02-02 20:39:49 +0100

    Generate documentation

--- R/scramble.R
+++ R/scramble.R
@@ -1,16 +1,17 @@
 #' Scramble a dataset
 #'
-#' This is the workhorse function of the `scramble` package. It takes a dataset
-#' as input and returns a scrambled dataset. The scrambled dataset includes the
-#' same columns as the original dataset but these have been resampled in such a
-#' way that the associations between columns present in the original dataset
-#' are lost.
+#' This is the workhorse function of the `scramble` package. It takes
+#' a dataset as input and returns a scrambled dataset. The scrambled
+#' dataset includes the same columns as the original dataset but these
+#' have been resampled in such a way that the associations between
+#' columns present in the original dataset are lost.
 #' @param data Data.frame. The original data. No default.
-#' @check.identical Logical. If TRUE `scramble` checks that no observation in
-#' the original dataset is present in the scrambled dataset. The probability
-#' that this would happen is of course minimal, but this check is here as an
-#' fail-safe. Note that this operation may take a long time with large datasets,
-#' and can therefore be turned off at your own risk. Defaults to TRUE.
+#' @param check.identical Logical. If TRUE `scramble` checks that no
+#'     observation in the original dataset is present in the scrambled
+#'     dataset. The probability that this would happen is of course
+#'     minimal, but this check is here as an fail-safe. Note that this
+#'     operation may take a long time with large datasets, and can
+#'     therefore be turned off at your own risk. Defaults to TRUE.
 #' @import assertthat
 #' @export
 scramble <- function(data, check.identical = TRUE) {
