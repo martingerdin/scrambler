06f0f3ca13f87466d9def04778af2b599eed5a2c
Martin Gerdin Warnberg <martingerdin@gmail.com>
2021-02-02 21:32:13 +0100

    Implement check.identical argument

--- R/scramble.R
+++ R/scramble.R
@@ -5,32 +5,48 @@
 #' dataset includes the same columns as the original dataset but these
 #' have been resampled in such a way that the associations between
 #' columns present in the original dataset are lost.
-#' @param data Data.frame. The original data. No default.
+#'
+#' `synthesize` makes `scramble` create a syntethic dataset that
+#' includes the unique values of each column in the original dataset
+#' but in a completely random order. 
+#' 
+#' @param original.data Data.frame. The original data. No default.
 #' @param check.identical Logical. If TRUE `scramble` checks that no
 #'     observation in the original dataset is present in the scrambled
 #'     dataset. The probability that this would happen is of course
 #'     minimal, but this check is here as an fail-safe. Note that this
 #'     operation may take a long time with large datasets, and can
 #'     therefore be turned off at your own risk. Defaults to TRUE.
+#' @param synthesize Logical. If TRUE new data is generated using
+#'     unique values present in the dataset. See
+#' @return A `data.frame`.
 #' @import assertthat
 #' @export
-scramble <- function(data, check.identical = TRUE) {
-  ## Check arguments
-  assert_that(is.data.frame(data))
-  ## Scramble data
-  scrambled.data <- data
-  scrambled.data[] <- lapply(data, scramble_column)
-  ## Check that no observation is kept unchanged
-  if (check.identical) {
-    Hmisc::find.matches(scrambled.data, data)
-  }
-
-  ## Return
-  scrambled.data
+scramble <- function(original.data, check.identical = TRUE, synthesize = FALSE ) {
+    ## Check arguments
+    assert_that(is.data.frame(original.data))
+    ## Scramble data
+    scrambled.data <- original.data
+    scrambled.data[] <- lapply(original.data, scramble_column)
+    ## Check that no observation is remains unchanged
+    if (check.identical)
+        any_identical(original.data, scrambled.data)
+    ## Return
+    scrambled.data
 }
 
 scramble_column <- function(column) {
-  n <- length(column)
-  scrambled.column <- sample(column, n)
-  scrambled.column
+    n <- length(column)
+    scrambled.column <- sample(column, n)
+    scrambled.column
+}
+
+any_identical <- function(original.data, scrambled.data) {
+    matches <- apply(original.data, 1, function(original.row) {
+        apply(scrambled.data, 1, function(scrambled.row) {
+            if (all(original.row == scrambled.row))
+                stop ("Observations from the original data still exist in the scrambled data, try again.")
+        })
+    })
+    invisible(FALSE)
 }
--- tests/testthat/test-scramble.R
+++ tests/testthat/test-scramble.R
@@ -2,9 +2,20 @@ context("Scramble")
 library(scrambler)
 
 test_that("scramble returns data.frame", {
-  expect_s3_class(scramble(test.data), "data.frame")
+  expect_s3_class(scramble(test.data, check.identical = FALSE), "data.frame")
 })
 
 test_that("scramble returns a data.frame that is different from the original", {
-  expect_false(identical(scramble(test.data), test.data))
+  expect_false(identical(scramble(test.data, check.identical = FALSE), test.data))
 })
+
+test_that("any_identical correctly detects remaining original observation", {
+    original.data <- test.data[1:10,]
+    scrambled.data <- original.data
+    scrambled.data[] <- lapply(original.data, scramble_column)
+    scrambled.data[10, ] <- original.data[10, ]
+    expect_error(any_identical(original.data, scrambled.data),
+                 "Observations from the original data still exist in the scrambled data, try again.")
+})
+
+rm(list = ls())
